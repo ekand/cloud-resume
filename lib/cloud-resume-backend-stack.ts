ef248aa16c56d37344fd4a457667ec69a32de601
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-15 20:59:24 -0600

    set up static html hosting with cloudfront and route 53

--- lib/cloud-resume-backend-stack.ts
+++ lib/cloud-resume-backend-stack.ts
@@ -1,19 +1,57 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Duration, Stack, StackProps } from "aws-cdk-lib";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3deploy from "aws-cdk-lib/aws-s3-deployment";
+import * as sns from "aws-cdk-lib/aws-sns";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as targets from "aws-cdk-lib/aws-route53-targets";
+import { Construct } from "constructs";
 
 export class CloudResumeBackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CloudResumeBackendQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const assetsBucket = new s3.Bucket(this, "CloudResumeBackendBucket", {
+      websiteIndexDocument: "index.html",
     });
 
-    const topic = new sns.Topic(this, 'CloudResumeBackendTopic');
+    new s3deploy.BucketDeployment(this, "DeployWebsite", {
+      sources: [s3deploy.Source.asset("./website-dist")],
+      destinationBucket: assetsBucket,
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      "Certificate",
+      // found using aws acm list-certificates --region us-east-1
+      "arn:aws:acm:us-east-1:212702451742:certificate/abf2dd8b-3651-4fe1-9452-56ade04917e1"
+    );
+
+    const cf = new cloudfront.Distribution(this, "cdnDistribution", {
+      defaultBehavior: { origin: new origins.S3Origin(assetsBucket) },
+      domainNames: ["www.erikresume.com", "erikresume.com"],
+      certificate,
+    });
+
+    const zone = route53.HostedZone.fromHostedZoneAttributes(
+      this,
+      "erikresume-zone",
+      {
+        zoneName: "erikresume.com",
+        hostedZoneId: "Z0110113OR9PYKNKIA91",
+      }
+    );
+
+    new route53.ARecord(this, "CDNARecord", {
+      zone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cf)),
+    });
+
+    new route53.AaaaRecord(this, "AliasRecord", {
+      zone,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(cf)),
+    });
   }
 }
