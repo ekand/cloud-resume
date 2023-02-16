09604d2690894f23c262af357ddcdde3f80a7213
Erik Anderson <erik.kristofer.anderson@gmail.com>
2023-02-15 21:19:06 -0600

    add styles and js, and reconfigure cloudfront permissions

--- lib/cloud-resume-backend-stack.ts
+++ lib/cloud-resume-backend-stack.ts
@@ -7,15 +7,31 @@ import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import * as route53 from "aws-cdk-lib/aws-route53";
 import * as targets from "aws-cdk-lib/aws-route53-targets";
+import * as iam from "aws-cdk-lib/aws-iam";
+
 import { Construct } from "constructs";
 
 export class CloudResumeBackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    const oai = new cloudfront.OriginAccessIdentity(this, "MyOAI");
+
     const assetsBucket = new s3.Bucket(this, "CloudResumeBackendBucket", {
       websiteIndexDocument: "index.html",
     });
+
+    assetsBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        resources: [assetsBucket.arnForObjects("*")],
+        principals: [oai.grantPrincipal],
+        conditions: {
+          StringLike: { "aws:Referer": `https://*.cloudfront.net/*` },
+        },
+        effect: iam.Effect.ALLOW,
+      })
+    );
 
     new s3deploy.BucketDeployment(this, "DeployWebsite", {
       sources: [s3deploy.Source.asset("./website-dist")],
@@ -28,12 +44,29 @@ export class CloudResumeBackendStack extends Stack {
       // found using aws acm list-certificates --region us-east-1
       "arn:aws:acm:us-east-1:212702451742:certificate/abf2dd8b-3651-4fe1-9452-56ade04917e1"
     );
-
-    const cf = new cloudfront.Distribution(this, "cdnDistribution", {
-      defaultBehavior: { origin: new origins.S3Origin(assetsBucket) },
-      domainNames: ["www.erikresume.com", "erikresume.com"],
-      certificate,
-    });
+    const cf = new cloudfront.CloudFrontWebDistribution(
+      this,
+      "MyDistribution",
+      {
+        originConfigs: [
+          {
+            s3OriginSource: {
+              s3BucketSource: assetsBucket,
+              originAccessIdentity: oai,
+            },
+            behaviors: [
+              {
+                isDefaultBehavior: true,
+                allowedMethods:
+                  cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+                cachedMethods:
+                  cloudfront.CloudFrontAllowedCachedMethods.GET_HEAD,
+              },
+            ],
+          },
+        ],
+      }
+    );
 
     const zone = route53.HostedZone.fromHostedZoneAttributes(
       this,
